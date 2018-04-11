b37d2e817d4698d4d465d87c97fd1922a538b28c
spartacusserdan <38261840+spartacusserdan@users.noreply.github.com>
2018-04-11 23:50:56 +0300

    Update iatrace.js

--- iatrace.js
+++ iatrace.js
@@ -1,3 +1,42 @@
+
 define(['/iatez/iat.js'], function(iatExtension){
-    return iatExtension({});
+    return iatExtension({
+        category1 : {
+            name : 'Black People', //Will appear in the data.
+            title : {
+                media : {word : 'Black People'}, //Name of the category presented in the task.
+                css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+                height : 4 //Used to position the "Or" in the combined block.
+            }, 
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {image: 'bf14_nc.jpg'},
+                {image: 'bf23_nc.jpg'},
+                {image: 'bm23_nc.jpg'},
+                {image: 'bm56_nc.jpg'}
+            ]
+        },    
+        category2 :    {
+            name : 'White People', //Will appear in the data.
+            title : {
+                media : {word : 'White People'}, //Name of the category presented in the task.
+                css : {color:'#31b404','font-size':'2em'}, //Style of the category title.
+                height : 4 //Used to position the "Or" in the combined block.
+            }, 
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {image: 'wf2_nc.jpg'},
+                {image: 'wf3_nc.jpg'},
+                {image: 'wm4_nc.jpg'},
+                {image: 'wm6_nc.jpg'}
+            ]
+        },
+        base_url : {//Where are your images at?
+            image : '/implicit/user/yba/pimraceiat/images/'
+        } 
+    });
+});
+
+define(['pipAPI','pipScorer','underscore'], function(API.addTasksSet({
+    iat : [{
+        type: 'pip', name: 'iat', version: '0.3', scriptUrl: 'iatrace.js'
+    }]
 });
