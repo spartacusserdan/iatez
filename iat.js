e5b45343c570a7b239ef002c1d89eaa3ed461204
spartacusserdan <38261840+spartacusserdan@users.noreply.github.com>
2018-04-11 23:57:34 +0300

    Delete iat.js

--- iat.js
+++
@@ -1,9 +0,0 @@
-define(['/implicit/common/all/js/PIP/piscripts/iat/iat.js'], function(iatExtension){
-    return iatExtension({});
-});
-
-define(['piManager','pipAPI','pipScorer','underscore'], function(API.addTasksSet({
-    iat : [{
-        type: 'pip', name: 'iat', version: '0.3', scriptUrl: 'iatrace.js'
-    }]
-});
