98c4f3a36f292272c435afb98961bbab168ec63b
spartacusserdan <38261840+spartacusserdan@users.noreply.github.com>
2018-04-12 00:10:45 +0300

    Update pip .js

--- pip .js
+++ pip .js
@@ -1,5 +1,5 @@
 efine(['pipAPI','pipScorer','underscore'], function(API.addTasksSet({
     iat : [{
-        type: 'pip', name: 'iat', version: '0.3', scriptUrl: 'iatrace.js'
+        type: 'pip', name: 'iat', version: '0.3', scriptUrl: 'iatez.js'
     }]
 });
